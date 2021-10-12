43060a1f1cb102122a740893290b04b9c6e428e8
godwin olele <godwinolele10@gmail.com>
2021-10-12 13:27:46 +0100

    working on main section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,11 +8,33 @@ export default function Home() {
             <div className="box1">
                 <p className="first-p">Do you need a quick loan?</p><br />
                 <h1>Credit~Link investment can help</h1><br />
-                <p className="second-p">You dont have to get stranded when you suddenly run out of casy.
+                <p className="second-p">You dont have to get stranded when you suddenly run out of cash.
                 Madas got you covered with fast emergency loans to meet your most pressing needs.
                 </p>
             </div>
             <div className="box2"></div>
+      </div>
+      <div className="main-contianer">
+          <div className="first-container">
+            <div className="box-1">
+              <div className="qpp-npf-container">
+              <div className="qpp">
+                <p><i class="far fa-credit-card"></i> Quick Payment Process</p>
+              </div>
+              <div className="npf">
+                <p><i class="far fa-thumbs-down"></i> No Prepayment Fees</p>
+              </div>
+              </div>
+              <div className="text-container">
+ <p className="first-p">Do you need a quick loan?</p><br />
+                <h1>Credit~Link investment can help</h1><br />
+                <p className="second-p">You dont have to get stranded when you suddenly run out of cash.
+                Madas got you covered with fast emergency loans to meet your most pressing needs.
+                </p>
+              </div>
+            </div>
+            <div className="box-2"></div>
+          </div>
       </div>
     </>
   );
