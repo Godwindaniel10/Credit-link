978a6d16a8bdd587266db6a1dc2cf0a2133788b2
godwin olele <godwinolele10@gmail.com>
2021-10-12 02:56:03 +0100

    First commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className="header-container">
+            <div className="box1">
+                <p className="first-p">Do you need a quick loan?</p><br />
+                <h1>Credit~Link investment can help</h1><br />
+                <p className="second-p">You dont have to get stranded when you suddenly run out of casy.
+                Madas got you covered with fast emergency loans to meet your most pressing needs.
+                </p>
+            </div>
+            <div className="box2"></div>
+      </div>
+    </>
+  );
+}
