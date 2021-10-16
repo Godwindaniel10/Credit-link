86f634482d4943840fab8a8ce50005906add14f1
godwin olele <godwinolele10@gmail.com>
2021-10-16 07:19:54 +0100

    login done

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,5 +2,25 @@ import React from 'react';
 import '../../App.css';
 
 export default function Login() {
-  return <h1 className='login'>Login</h1>;
+  return (
+    <>
+    <div className="login-container">
+      <div className="login-container2">
+      <div className="header-intro">
+        <h1>Credit Link Finance</h1>
+        <p>Sign in to start your session</p>
+      </div>
+      <form>
+        <input type="text" name="email-phone" id="email-phone" placeholder="Email or Phone number"/><br /><br />
+        <input type="password" name="password" id="password" placeholder="Password"/><br /><br />
+        <div className="fpass-sign-in-container">
+          <a href="" className="fpassword">Forgotten password?</a>
+          <a href="" className="sign-in">sign in</a>
+        </div>
+       <p className="dhac">Dont have an account? <a href="">Sign up</a></p>
+      </form>
+      </div>
+    </div>
+    </>
+  )
 }
